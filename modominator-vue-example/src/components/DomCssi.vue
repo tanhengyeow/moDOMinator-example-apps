f94b0ffed42478191cf38b633e1be20f3b27b1f5
takuyakanbr <takuyakanbr@gmail.com>
2019-11-10 17:03:32 +0800

    Add DOM CSSi page for Vue example

--- modominator-vue-example/src/components/DomCssi.vue
+++ modominator-vue-example/src/components/DomCssi.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
     <h1 class="title">DOM CSS Injection</h1>
+    <p>
+      The widget below is vulnerable to injection of CSS styles.<br/>
+      moDOMinator should be able to automatically detect and attack the input element below.
+    </p>
+    <br/>
 
-    <p>Hello world.</p>
+    <div class="box">
+      <h5 class="title is-5">What is your favorite color?</h5>
+      <input
+        class="input"
+        placeholder="#000000"
+        v-model="color"
+      />
+      <template v-if="color">
+        <br/>
+        <br/>
+        <div class="color-display" :style="`background-color: ${color}`"></div>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -10,6 +27,20 @@
   import Vue from 'vue';
 
   export default Vue.extend({
-    name: 'DomCssi'
+    name: 'DomCssi',
+    data() {
+      return {
+        color: ''
+      };
+    }
   });
 </script>
+
+<style>
+  .color-display {
+    width: 400px;
+    height: 100px;
+    border: 1px solid #bbb;
+    border-radius: 15px;
+  }
+</style>
